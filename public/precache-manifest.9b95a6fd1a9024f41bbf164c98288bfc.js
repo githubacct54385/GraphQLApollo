2caeb4edac98b8f11b42c14f34fc1f890cf3962f
Alex Barke <alex.barke@csintelligence.asia>
2020-02-15 22:03:53 +0800

    commit before heroku push

---
+++ public/precache-manifest.9b95a6fd1a9024f41bbf164c98288bfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b80990d3f21d5ce3c5c2f4554a669f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e091e2358b59bf54f1cb",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2564afdf46afb014765f",
+    "url": "/static/js/2.70d11454.chunk.js"
+  },
+  {
+    "revision": "a0c18f8786874a74d6fd4d46260562f6",
+    "url": "/static/js/2.70d11454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e091e2358b59bf54f1cb",
+    "url": "/static/js/main.816e0504.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
