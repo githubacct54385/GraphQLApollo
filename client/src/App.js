3f0f83f52c3fd05f5fd6389e23f96933ca293b36
Alex Barke <alex.barke@csintelligence.asia>
2020-02-15 21:51:38 +0800

    finished part 3 of apollo course

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import "./App.css";
+import logo from "../src/logo.png";
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
